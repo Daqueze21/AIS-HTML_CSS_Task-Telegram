f9502350fb199a14743c1f8485ba87a6c308230a
Dzmitry Bryt <bdv212@yandex.ru>
2020-07-26 15:29:42 +0300

    feat: add chats section with styles, add js file

---
+++ index.js
@@ -0,0 +1,32 @@
+class CreateChats {
+   constructor() {
+      this.$el = document.querySelector('.chats__list-scroll ul');
+      this.html = `
+         <li class="chats__list-item">
+            <div class="item-icon">
+               <img src='./img/img_avatar3.png' alt="" class="item-avatar">
+            </div>
+            <div class="item-description">
+               <p class="item-name">Some chat name</p>
+               <p class="item-msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ullam.</p>
+            </div>
+            <div class="item-meta">
+               <p class="msg-time">8:12</p>
+               <p class="unread-counter">26</p>
+            </div>
+         </li>
+      `;
+   }
+
+   renderElements(numb) {
+      for(let i = 0; i <= numb; i++) {
+         this.$el.insertAdjacentHTML('afterend', this.html);
+      }
+   }
+}
+
+
+window.addEventListener('load', () => {
+   const chats = new CreateChats();
+   chats.renderElements(20);
+});
